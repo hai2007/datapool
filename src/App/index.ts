e20c888a2d7a05dee67673e5c6ce07559ea46411
你好2007 <2501482523@qq.com>
2022-03-05 15:41:39 +0800

    完成对已有地图数据的测试

--- src/App/index.ts
+++ src/App/index.ts
@@ -3,11 +3,38 @@ import { Component } from 'nefbl'
 import style from './index.scss'
 import template from './index.html'
 
+import Clunch from 'clunch'
+import map from '@clunch/map/index'
+
+Clunch.series('ui-map', map)
+
+import image from './index.clunch'
+
+// 中国省地图数据
+// import mapGeoJSON from '../../china.geoJSON/china.geoJSON.js'
+
+// 香港区地图数据
+import mapGeoJSON from '../../HK.geoJSON/hk.geoJSON.js'
+
 @Component({
     selector: "app-root",
     template,
     styles: [style]
 })
 export default class {
+
+    $mounted() {
+
+        new Clunch({
+            el: document.getElementById('canvas'),
+            render: image,
+            data() {
+                return {
+                    mapGeoJSON
+                }
+            }
+        })
+
+    }
 
 }
