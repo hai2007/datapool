de69705107b2f460df406cd463f24ef7ee5ca416
你好2007 <2501482523@qq.com>
2022-03-15 10:21:59 +0800

    新增广东市级地图数据

---
+++ Dongguan.geoJSON/dongguan.geoJSON.js
@@ -0,0 +1,7 @@
+var DongguanGeoJSON = {"type":"FeatureCollection","features":[]};
+
+       if (typeof module === "object" && typeof module.exports === "object") {
+           module.exports = DongguanGeoJSON;
+       } else {
+           window.DongguanGeoJSON = DongguanGeoJSON;
+       }
---
+++ Zhongshan.geoJSON/zhongshan.geoJSON.js
@@ -0,0 +1,7 @@
+var ZhongshanGeoJSON = {"type":"FeatureCollection","features":[]};
+
+       if (typeof module === "object" && typeof module.exports === "object") {
+           module.exports = ZhongshanGeoJSON;
+       } else {
+           window.ZhongshanGeoJSON = ZhongshanGeoJSON;
+       }
