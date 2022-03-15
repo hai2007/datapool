9cea937a2ecc4760baa1ae6eaa62ae0e34417d74
你好2007 <2501482523@qq.com>
2022-03-15 09:54:05 +0800

    添加河南的市级地图数据

--- .script/geoJSON/bin.js
+++ .script/geoJSON/bin.js
@@ -5,56 +5,59 @@
  * xxxxxxx_4  日期
  */
 
- const fs = require('fs');
- const nodejs = require("@hai2007/nodejs");
+const fs = require('fs');
+const nodejs = require("@hai2007/nodejs");
 
- const list = {
-    '370100-济南市': ['jinan', 'Jinan', '济南'],
-    '370200-青岛市': ['qingdao', 'Qingdao', '青岛'],
-    '370300-淄博市': ['zibo', 'Zibo', '淄博'],
-    '370400-枣庄市': ['zaozhuang', 'Zaozhuang', '枣庄'],
-    '370500-东营市': ['doy', 'Doy', '东营'],
-    '370600-烟台市': ['yantai', 'Yantai', '烟台'],
-    '370700-潍坊市': ['weifang', 'Weifang', '潍坊'],
-    '370800-济宁市': ['jining', 'Jining', '济宁'],
-    '370900-泰安市': ['taian', 'Taian', '泰安'],
-    '371000-威海市': ['weihai', 'Weihai', '威海'],
-    '371100-日照市': ['rizhao', 'Rizhao', '日照'],
-    '371300-临沂市': ['linyi', 'Linyi', '临沂'],
-    '371400-德州市': ['texas', 'Texas', '德州'],
-    '371500-聊城市': ['liaocheng', 'Liaocheng', '聊城'],
-    '371600-滨州市': ['binzhou', 'Binzhou', '滨州'],
-    '371700-菏泽市': ['heze', 'Heze', '菏泽'],
- };
+const list = {
+    '410100-郑州市': ['', 'Zhengzhou', '郑州'],
+    '410200-开封市': ['', 'Kaifeng', '开封'],
+    '410300-洛阳市': ['', 'Luoyang', '洛阳'],
+    '410400-平顶山市': ['', 'Pingdingshan', '平顶山'],
+    '410500-安阳市': ['', 'Anyang', '安阳'],
+    '410600-鹤壁市': ['', 'Hebi', '鹤壁'],
+    '410700-新乡市': ['', 'Xinxiang', '新乡'],
+    '410800-焦作市': ['', 'Jiaozuo', '焦作'],
+    '410900-濮阳市': ['', 'Puyang', '濮阳'],
+    '411000-许昌市': ['', 'Xuchang', '许昌'],
+    '411100-漯河市': ['', 'Luohe', '漯河'],
+    '411200-三门峡市': ['', 'Sanmenxia', '三门峡'],
+    '411300-南阳市': ['', 'Nanyang', '南阳'],
+    '411400-商丘市': ['', 'Shangqiu', '商丘'],
+    '411500-信阳市': ['', 'Xinyang', '信阳'],
+    '411600-周口市': ['', 'Zhoukou', '周口'],
+    '411700-驻马店市': ['', 'Zhumadian', '驻马店'],
+};
 
- let month = new Date().getMonth() + 1;
- let day = new Date().getDate();
- let _date_ = (new Date().getFullYear()) + "-" + (month > 9 ? "" : "0") + month + "-" + (day > 9 ? "" : "0") + day;
- let mdlink = '';
+let month = new Date().getMonth() + 1;
+let day = new Date().getDate();
+let _date_ = (new Date().getFullYear()) + "-" + (month > 9 ? "" : "0") + month + "-" + (day > 9 ? "" : "0") + day;
+let mdlink = '';
 
- for (let key in list) {
-     let item = list[key];
+for (let key in list) {
+    let item = list[key];
 
-     let src = 'C:\/Users\/yelloxing\/Desktop\/chinaGeoJson-master\/city\/' + key + ".json";
-     // let src = '../chinaGeoJson-master/city/' + key + ".json";
+    if (item[0] == "") item[0] = (item[1] + "").toLowerCase();
 
-     //  判断文件是否已经存在
-     if (fs.existsSync('./' + item[1] + '.geoJSON')) {
-         console.log(item);
-         continue;
-     }
+    let src = 'C:\/Users\/yelloxing\/Desktop\/chinaGeoJson-master\/city\/' + key + ".json";
+    // let src = '../chinaGeoJson-master/city/' + key + ".json";
 
-     nodejs.copySync("./.script/geoJSON/template", './' + item[1] + '.geoJSON');
+    //  判断文件是否已经存在
+    if (fs.existsSync('./' + item[1] + '.geoJSON')) {
+        console.log(item);
+        continue;
+    }
 
-     for (let file of ['package.json', 'README.md']) {
-         fs.writeFileSync('./' + item[1] + '.geoJSON/' + file,
-             (fs.readFileSync('./' + item[1] + '.geoJSON/' + file) + "")
-                 .replace(/xxxxxxx\_1/g, item[0]).replace(/xxxxxxx\_2/g, item[1]).replace(/xxxxxxx\_3/g, item[2]).replace(/_$/, ''));
-     }
+    nodejs.copySync("./.script/geoJSON/template", './' + item[1] + '.geoJSON');
 
-     fs.writeFileSync('./' + item[1] + ".geoJSON/CHANGELOG", (fs.readFileSync('./' + item[1] + ".geoJSON/CHANGELOG") + "").replace(/xxxxxxx\_4/, _date_));
+    for (let file of ['package.json', 'README.md']) {
+        fs.writeFileSync('./' + item[1] + '.geoJSON/' + file,
+            (fs.readFileSync('./' + item[1] + '.geoJSON/' + file) + "")
+                .replace(/xxxxxxx\_1/g, item[0]).replace(/xxxxxxx\_2/g, item[1]).replace(/xxxxxxx\_3/g, item[2]).replace(/_$/, ''));
+    }
 
-     fs.writeFileSync('./' + item[1] + '.geoJSON/' + item[0] + ".geoJSON.js", `var ${item[1]}GeoJSON = ${fs.readFileSync(src)};
+    fs.writeFileSync('./' + item[1] + ".geoJSON/CHANGELOG", (fs.readFileSync('./' + item[1] + ".geoJSON/CHANGELOG") + "").replace(/xxxxxxx\_4/, _date_));
+
+    fs.writeFileSync('./' + item[1] + '.geoJSON/' + item[0] + ".geoJSON.js", `var ${item[1]}GeoJSON = ${fs.readFileSync(src)};
 
        if (typeof module === "object" && typeof module.exports === "object") {
            module.exports = ${item[1]}GeoJSON;
@@ -62,8 +65,8 @@
            window.${item[1]}GeoJSON = ${item[1]}GeoJSON;
        }`);
 
-     mdlink += "- [" + item[2] + "](./" + item[1] + ".geoJSON/README.md)\n"
+    mdlink += "- [" + item[2] + "](./" + item[1] + ".geoJSON/README.md)\n"
 
- }
+}
 
- console.log(mdlink);
+console.log(mdlink);
---
+++ Sanmenxia.geoJSON/README.md
@@ -0,0 +1,37 @@
+# Sanmenxia.geoJSON
+🗄️ 三门峡地图数据。
+
+## Issues
+使用的时候遇到任何问题或有好的建议，请点击进入[issue](https://github.com/hai2007/datapool/issues)，欢迎参与维护！
+
+## How to use?
+
+请直接在页面中加入下面这行代码，然后通过```SanmenxiaGeoJSON```即可调用：
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/@datapool/sanmenxia.geojson@1"></script>
+```
+
+如果你想通过npm方式管理，首先你需要通过命令行安装``````SanmenxiaGeoJSON``````，就像这样：
+
+```bash
+npm install --save @datapool/sanmenxia.geojson
+```
+
+安装好了以后，在需要的地方引入即可：
+
+```js
+import SanmenxiaGeoJSON from '@datapool/sanmenxia.geojson';
+```
+
+或
+
+```js
+const SanmenxiaGeoJSON = require("@datapool/sanmenxia.geojson");
+```
+
+开源协议
+---------------------------------------
+[MIT](https://github.com/hai2007/datapool/blob/master/LICENSE)
+
+Copyright (c) 2022 [hai2007](https://hai2007.gitee.io/sweethome/) 走一步，再走一步。
